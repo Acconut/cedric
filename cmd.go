db5ed767ccfb569a6eac9dc777a14fa4be1819b6
Marius <maerious@gmail.com>
2016-01-10 14:01:01 +0100

    Format code

--- cmd.go
+++ cmd.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-  "io/ioutil"
-  "os"
-  "fmt"
-  "go/build"
-  "strings"
-  "path/filepath"
-  "text/template"
-  "flag"
+	"flag"
+	"fmt"
+	"go/build"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
 )
 
 var outputTemplate = template.Must(template.New("script").Parse(
-`{{$gopath := .Tmp}}{{$cwd := .Cwd}}# Automatically abort on error
+	`{{$gopath := .Tmp}}{{$cwd := .Cwd}}# Automatically abort on error
 set -e
 
 # Remove currently installed vendored dependencies
@@ -85,103 +85,103 @@ rm -rf {{.Tmp}}
 `))
 
 type templateInput struct {
-  Cwd string
-  Tmp string
-  Packages []string
-  AddSubmodules bool
-  Package string
+	Cwd           string
+	Tmp           string
+	Packages      []string
+	AddSubmodules bool
+	Package       string
 }
 
 func main() {
-  var destPath string
-  var recursive bool
-  var addSubmodules bool
-  flag.StringVar(&destPath, "directory", "./", "Directory to analyse dependencies and download to")
-  flag.BoolVar(&recursive, "recursive", true, "Indicates whether current project should be analysed recursively")
-  flag.BoolVar(&addSubmodules, "submodules", true, "Indicates whether dependencies should be added as Git submodules")
+	var destPath string
+	var recursive bool
+	var addSubmodules bool
+	flag.StringVar(&destPath, "directory", "./", "Directory to analyse dependencies and download to")
+	flag.BoolVar(&recursive, "recursive", true, "Indicates whether current project should be analysed recursively")
+	flag.BoolVar(&addSubmodules, "submodules", true, "Indicates whether dependencies should be added as Git submodules")
 
-  flag.Parse()
+	flag.Parse()
 
-  destPathAbs, err := filepath.Abs(destPath)
-  handleErr(err)
+	destPathAbs, err := filepath.Abs(destPath)
+	handleErr(err)
 
-  pkgImportPath := ""
+	pkgImportPath := ""
 
-  if GOPATH := os.Getenv("GOPATH"); GOPATH != "" {
-    if !os.IsPathSeparator(GOPATH[len(GOPATH)-1]) {
-      GOPATH += string(os.PathSeparator)
-    }
+	if GOPATH := os.Getenv("GOPATH"); GOPATH != "" {
+		if !os.IsPathSeparator(GOPATH[len(GOPATH)-1]) {
+			GOPATH += string(os.PathSeparator)
+		}
 
-    GOPATH += "src/"
+		GOPATH += "src/"
 
-    if strings.HasPrefix(destPathAbs, GOPATH) {
-      pkgImportPath = destPathAbs[len(GOPATH):]
-    }
-  }
+		if strings.HasPrefix(destPathAbs, GOPATH) {
+			pkgImportPath = destPathAbs[len(GOPATH):]
+		}
+	}
 
-  externalImports := make([]string, 0)
+	externalImports := make([]string, 0)
 
-  walkFunc := func(path string, info os.FileInfo, err error) error {
-    handleErr(err)
+	walkFunc := func(path string, info os.FileInfo, err error) error {
+		handleErr(err)
 
-    base := filepath.Base(path)
-    // Exclude special directories from analysing
-    if base == ".git" || base == "vendor" {
-      return filepath.SkipDir
-    }
+		base := filepath.Base(path)
+		// Exclude special directories from analysing
+		if base == ".git" || base == "vendor" {
+			return filepath.SkipDir
+		}
 
-    if !info.IsDir() {
-      return nil
-    }
+		if !info.IsDir() {
+			return nil
+		}
 
-    _, err = build.ImportDir(path, 0)
-    if _, ok := err.(*build.NoGoError); !ok {
-      handleErr(err)
-    } else {
-      // Ignore directory if no Go source files are in there
-      return nil
-    }
+		_, err = build.ImportDir(path, 0)
+		if _, ok := err.(*build.NoGoError); !ok {
+			handleErr(err)
+		} else {
+			// Ignore directory if no Go source files are in there
+			return nil
+		}
 
-    externalImports = append(externalImports, "./" + path)
+		externalImports = append(externalImports, "./"+path)
 
-    return nil
-  }
+		return nil
+	}
 
-  if recursive {
-    handleErr(filepath.Walk(destPath, walkFunc))
-  } else {
-    info, err := os.Stat(destPath)
-    handleErr(err)
-    handleErr(walkFunc(destPath, info, nil))
-  }
+	if recursive {
+		handleErr(filepath.Walk(destPath, walkFunc))
+	} else {
+		info, err := os.Stat(destPath)
+		handleErr(err)
+		handleErr(walkFunc(destPath, info, nil))
+	}
 
-  if len(externalImports) == 0 {
-    return
-  }
+	if len(externalImports) == 0 {
+		return
+	}
 
-  cwd, err := os.Getwd()
-  handleErr(err)
+	cwd, err := os.Getwd()
+	handleErr(err)
 
-  // Create temporary directory to simulate empty GOPATH. `go get` will download
-  // the dependencies into this path where we later copy them from.
-  tmp, err := ioutil.TempDir("", "cedric-gopath-")
-  handleErr(err)
+	// Create temporary directory to simulate empty GOPATH. `go get` will download
+	// the dependencies into this path where we later copy them from.
+	tmp, err := ioutil.TempDir("", "cedric-gopath-")
+	handleErr(err)
 
-  outputTemplate.Execute(os.Stdout, templateInput{
-    Cwd: cwd,
-    Tmp: tmp,
-    Packages: externalImports,
-    AddSubmodules: addSubmodules,
-    Package: pkgImportPath,
-  })
+	outputTemplate.Execute(os.Stdout, templateInput{
+		Cwd:           cwd,
+		Tmp:           tmp,
+		Packages:      externalImports,
+		AddSubmodules: addSubmodules,
+		Package:       pkgImportPath,
+	})
 }
 
 func handleErr(err error) {
-  if err == nil {
-    return
-  }
+	if err == nil {
+		return
+	}
 
-  fmt.Fprintf(os.Stderr, "Internal error occured:\n\t%s\n", err)
+	fmt.Fprintf(os.Stderr, "Internal error occured:\n\t%s\n", err)
 
-  os.Exit(1)
+	os.Exit(1)
 }
